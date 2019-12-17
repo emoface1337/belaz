acdc5d53f534bb80bd56bde8b2b9776dc728002d
emoface1337 <CHVRCHES@mail.ru>
2019-12-17 14:40:42 +0300

    initialized belaz project

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+"use strict"
+
+// Load plugins
+const autoprefixer = require("gulp-autoprefixer")
+const gulp = require("gulp")
+const rename = require("gulp-rename")
+const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
+const browserSync = require('browser-sync')
+
+const paths = {
+    html: ['index.html'],
+    sass: ['./scss/**/*.sass', './scss/**/*.scss'],
+    js: ['./js/**/*.js']
+}
+
+const css = () => {
+    return gulp
+        .src(paths.sass)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest("./css/"))
+        .pipe(rename({suffix: ".min"}))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("./css/"))
+        .pipe(browsersync.stream())
+}
+
+const scripts = () => {
+    return gulp
+        .src(paths.js)
+        .pipe(uglify())
+        .pipe(browserSync.stream())
+}
+
+const watch = () => {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        },
+        port: 8081,
+        open: true,
+        notify: false
+    })
+    gulp.watch(paths.sass, css)
+    gulp.watch(paths.js, scripts)
+    gulp.watch("./*.html").on('change', browserSync.reload)
+}
+
+gulp.task('css', css)
+gulp.task('scripts', scripts)
+gulp.task('watch', watch)
+gulp.task('build', gulp.series(gulp.parallel(css, scripts)))
+
+gulp.task('default', gulp.series('build', 'watch'))
